17a07af5ca3336ef5bdd95b8739ffdad08ba64a4
hsnhsmr <hsnhsmr04@gmail.com>
2026-03-14 01:45:40 +0800

    commit

--- codes.R
+++ codes.R
@@ -1,22 +1,18 @@
 library(tidyquant); library(tidyverse)
 library(lubridate)
 
+#filter appearances
 appearances <- read.csv("appearances.csv")
 View(appearances)
 summary(appearances)
 str(appearances)
-#appearances$dates <- as.Date(appearances$date)
 
 start_date <- as.Date("2024-08-16")
 end_date <- as.Date("2025-05-25")
-
 appearances2 <- appearances[appearances$date >= start_date & appearances$date <= end_date,]
+View(appearances2)
 
 appearances3 <- appearances2 %>% 
-#  arrange(desc(dates)) %>%
-#  filter(filter(date >= start_date & date <= end_date)) %>%
   select(player_id, player_club_id, goals, assists, minutes_played, date)
 View(appearances3)
 
-player_valuations <- read.csv("player_valuations.csv")
-View(player_valuations)
