71e39f15ad51ea34dbee02f718a7277e3ac22f09
hsnhsmr <hsnhsmr04@gmail.com>
2026-03-17 16:52:15 +0800

    edit

--- codes.R
+++ codes.R
@@ -20,8 +20,6 @@ players2 <- merge(appearances2, players, by = "player_id")
 players2 <- players2 %>%
   mutate(age = floor(time_length(interval(players2$date_of_birth, "2025-05-25"), "years"))) %>%
   mutate(market_value = market_value_in_eur * 1.47) %>%
-  select(player_id, date_of_birth, position, sub_position, current_club_id, market_value, 
-         game_id, player_club_id, date, player_name, competition_id, minutes_played, age, goals, assists)
 View(players2)
 
 #adding club name
@@ -29,12 +27,13 @@ clubs <- read.csv("clubs.csv", header = TRUE)
 colnames(clubs)[1] <- "player_club_id"
 clubs <- select(clubs, player_club_id, name)
 View(clubs)
-players2 <- merge(clubs, players2, by = "player_club_id")
+players2 <- merge(clubs, players2, by = "player_club_id") %>%
+  select(sub_position, market_value, date, player_name, minutes_played, age, goals, assists)
 View(players2)
 
 #total minutes played, goals and assists by each player
 players3 <- players2 %>%
-  group_by(player_id) %>%
+  group_by(player_name) %>%
   summarise(total_minutes_played = sum(minutes_played),
             total_goals = sum(goals),
             total_assists = sum(assists))
